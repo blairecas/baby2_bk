ed7a4d6cedab37c3296200933f98c4f672c70cde
blairecas <chaminealex@mail.ru>
2026-02-08 11:55:23 +0300

    initial

---
+++ 1_compile_baby2.bat
@@ -0,0 +1,28 @@
+@echo off
+
+echo.
+echo ===========================================================================
+echo Graphics
+echo ===========================================================================
+php -f ./scripts/conv_graphics.php
+if %ERRORLEVEL% NEQ 0 ( exit /b )
+
+echo.
+echo ===========================================================================
+echo Code
+echo ===========================================================================
+set NAME=baby2
+php -f ../scripts/preprocess.php %NAME%.mac
+if %ERRORLEVEL% NEQ 0 ( exit /b )
+..\scripts\macro11 -ysl 32 -yus -l _%NAME%.lst _%NAME%.mac
+if %ERRORLEVEL% NEQ 0 ( exit /b )
+php -f ../scripts/lst2bin.php _%NAME%.lst ./release/%NAME%.bin bbk 1000
+if %ERRORLEVEL% NEQ 0 ( exit /b )
+php -f ../scripts/bin2wav.php ./release/%NAME%.bin
+if %ERRORLEVEL% NEQ 0 ( exit /b )
+
+del _%NAME%.mac
+rem del _%NAME%.lst
+
+echo.
+start ..\..\bkemu\BK_x64.exe /C BK-0010-01 /B .\release\%NAME%.bin
