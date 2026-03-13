07f2cb97780fdaac78b14cc41fe34a3c732b5c56
blairecas <chaminealex@mail.ru>
2026-03-13 14:05:32 +0300

    compression added

--- 1_compile_baby2.bat
+++ 1_compile_baby2.bat
@@ -1,5 +1,7 @@
 @echo off
 
+set NAME=baby2
+
 rem echo.
 rem echo ===========================================================================
 rem echo Graphics
@@ -15,18 +17,34 @@ echo.
 echo ===========================================================================
 echo Code
 echo ===========================================================================
-set NAME=baby2
+php -f ../scripts/preprocess.php %NAME%_cpu.mac
+if %ERRORLEVEL% NEQ 0 ( exit /b )
+..\scripts\macro11 -ysl 32 -yus -l _%NAME%_cpu.lst _%NAME%_cpu.mac
+if %ERRORLEVEL% NEQ 0 ( exit /b )
+php -f ../scripts/lst2bin.php _%NAME%_cpu.lst %NAME%_cpu.bin bin 1000
+if %ERRORLEVEL% NEQ 0 ( exit /b )
+
+echo.
+echo ===========================================================================
+echo Loader, Packing etc..
+echo ===========================================================================
+del %NAME%_cpu.zx0
+..\scripts\zx0 %NAME%_cpu.bin %NAME%_cpu.zx0
+if %ERRORLEVEL% NEQ 0 ( exit /b )
 php -f ../scripts/preprocess.php %NAME%.mac
 if %ERRORLEVEL% NEQ 0 ( exit /b )
 ..\scripts\macro11 -ysl 32 -yus -l _%NAME%.lst _%NAME%.mac
 if %ERRORLEVEL% NEQ 0 ( exit /b )
 php -f ../scripts/lst2bin.php _%NAME%.lst ./release/%NAME%.bin bbk 1000
 if %ERRORLEVEL% NEQ 0 ( exit /b )
-php -f ../scripts/bin2wav.php ./release/%NAME%.bin
-if %ERRORLEVEL% NEQ 0 ( exit /b )
 
+
+del _%NAME%_cpu.mac
+del %NAME%_cpu.bin
+del %NAME%_cpu.zx0
 del _%NAME%.mac
-rem del _%NAME%.lst
+del _%NAME%.lst
+rem del _%NAME%_cpu.lst
 
 echo.
 start ..\..\bkemu\BK_x64.exe /C BK-0010-01 /B .\release\%NAME%.bin
